868076bd734a2c8a1ac4d5fb93147b0b77daaa77
kikurapa <56951000+kikurapa@users.noreply.github.com>
2024-01-06 17:33:42 +0530

    Add files via upload

---
+++ Technology/Microservice Assigments/Assigment1/PatientManagement/src/repo/ServiceRepository.java
@@ -0,0 +1,12 @@
+package com.wipro.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wipro.model.Services;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Services, Integer>{
+
+
+}
