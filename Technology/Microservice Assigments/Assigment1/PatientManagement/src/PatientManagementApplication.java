f9a1becf5753e94e859bd917151d00c890d44b40
kikurapa <56951000+kikurapa@users.noreply.github.com>
2024-01-06 17:25:26 +0530

    Add files via upload

--- Technology/Microservice Assigments/Assigment1/PatientManagement/src/PatientManagementApplication.java
+++ Technology/Microservice Assigments/Assigment1/PatientManagement/src/PatientManagementApplication.java
@@ -1,1 +1,13 @@
+package com.wipro;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementApplication.class, args);
+	}
+
+}
